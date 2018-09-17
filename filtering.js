9edfb3342e18e9187938a9e0633aaf28621fe13c
Zuber <zuberanwar@googlemail.com>
2018-09-17 22:30:05 +0100

    now with default display

--- filtering.js
+++ filtering.js
@@ -3,6 +3,8 @@ const parent = document.querySelector(".characters");
 let names
 let filterarray = []
 
+
+
 fetch('https://swapi.co/api/people')
 .then(function(response) {
   return response.json();
@@ -10,10 +12,14 @@ fetch('https://swapi.co/api/people')
 })
 .then(function(body) {
   names = body.results
-})
+  filterarray = names.map(function(character){
+    return `<li>${character.name}</li>`}).join("")
+    parent.innerHTML = filterarray;
+  })
 
 
 form.addEventListener("input", function(event){
+  parent.innerHTML = ""
   filterarray = names.filter(function(char){
     return char.name.toLowerCase().includes(event.target.value.toLowerCase())
   }).map(function(character){
